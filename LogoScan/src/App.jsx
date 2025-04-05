7c40ac02f06cbc709481a20589dec48b9b7c17e2
ImperialKoi <imperialkoi9@gmail.com>
2025-04-05 10:16:04 -0400

    literally everything

--- LogoScan/src/App.jsx
+++ LogoScan/src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import CameraPage from "./pages/CameraPage";
 import GalleryPage from "./pages/GalleryPage";
+import Login from './pages/Login';
+import HomePage from './pages/HomePage';
+import Home from './pages/Home';
 
 function App() {
     return (
         <Routes>
-          <Route path="/" element={<CameraPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/camera" element={<CameraPage />} />
           <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
     );
 }
